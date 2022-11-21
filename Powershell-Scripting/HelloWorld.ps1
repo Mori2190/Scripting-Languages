1a184d05ed080377c65274c35ca28e5f91800381
Forrest Born <fborn2190@gmail.com>
2022-11-21 15:43:31 -0800

    Organization and Cleanup

--- Powershell-Scripting/HelloWorld.ps1
+++
@@ -1,3 +0,0 @@
-# New-Item HelloWorld.ps1 // New-Item creates .ps1 file in current directory
-# code HelloWorld.ps1 // code command opens the powershell file to write and edit the script
-Write-Output 'Hello World!'
