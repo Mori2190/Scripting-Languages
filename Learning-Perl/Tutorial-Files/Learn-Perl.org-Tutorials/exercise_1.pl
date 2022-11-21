5f31b5609121c95bd19b6e0eb7c8d7e43e950384
Forrest Born <97557895+Mori2190@users.noreply.github.com>
2022-11-21 15:49:53 -0800

    Consolidation

---
+++ Learning-Perl/Tutorial-Files/Learn-Perl.org-Tutorials/exercise_1.pl
@@ -0,0 +1,13 @@
+#Exercise 1
+=Use the print command to print the line "Hello, World!".
+Note that the new line character \n should be used in order to print a new line.
+=cut
+
+##########
+#GIVEN
+##########
+# Text to the right of a '#' is treated as a comment - below is the perl command
+print "Goodbye, World!\n";
+
+## Edited code:
+print "Hello, World!\n";
