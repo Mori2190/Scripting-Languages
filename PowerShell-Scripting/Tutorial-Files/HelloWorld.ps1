eba8c4c0e5492212479838c53efc4b37260b8941
Forrest Born <97557895+Mori2190@users.noreply.github.com>
2022-11-21 19:13:32 -0500

    Update to add source material as comment

--- PowerShell-Scripting/Tutorial-Files/HelloWorld.ps1
+++ PowerShell-Scripting/Tutorial-Files/HelloWorld.ps1
@@ -1,3 +1,5 @@
+# This tutorial file is created from Microsoft Learn: https://learn.microsoft.com/en-us/training/modules/powershell-write-first/2-exercise-hello-world
 # New-Item HelloWorld.ps1 // New-Item creates .ps1 file in current directory
 # code HelloWorld.ps1 // code command opens the powershell file to write and edit the script
+
 Write-Output 'Hello World!'
