aaff3243a8b53a74dd25739b4d55a1dee3f6d303
Forrest Born <fborn2190@gmail.com>
2024-08-30 12:35:25 -0400

    Update pst_archive.bat

--- Windows_Batch/pst_archive.bat
+++ Windows_Batch/pst_archive.bat
@@ -3,8 +3,8 @@ Echo "Closing Outlook."
 taskkill /IM "OUTLOOK.EXE"
 timeout /t 10
 Echo "Archiving .PST files."
-copy /Y "%userprofile%\Outlook Files\*.pst" "%userprofile%\Desktop\Archive\PSTs\*.archive"
-timeout /t 10
+copy /Y "%userprofile%\Outlook Files\*.pst" "%userprofile%\OneDrive\Documents\Outlook Files\*.pst.archive"
+timeout /t 5
 Echo "Opening Outlook."
 start /B "" "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE"
 Echo "Task Complete!"
